2b21cf0a9eff3bdbd40c4a8f4019ee708f0c3c2a
Raphael Fernandez <raphael.tomines.fernandez@gmail>
2026-02-09 11:11:39 +0800

    Added recipe2 note: links are broken

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import homepage, recipelist
+from .views import homepage, recipelist, recipe1, recipe2
 
 urlpatterns = [
     path('', homepage, name='homepage'),
-    path('list', recipelist, name='recipelist'),
+    path('recipes/list', recipelist, name='recipelist'),
+    path('1', recipe1, name='recipe1'),
+    path('2', recipe2, name='recipe2'),
 ]
 
 app_name = "ledger"
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -30,7 +30,7 @@ def recipelist(request):
                         "quantity": "1 packet"
                     }
                 ],
-                "link": "/recipe/1"
+                "link": "ledger/recipe/1"
             },
             {
                 "name": "Recipe 2",
@@ -64,9 +64,74 @@ def recipelist(request):
                         "quantity": "1 kilo"
                     }
                 ],
-                "link": "/recipe/2"
+                "link": "ledger/recipe/2"
             }
         ]
-    }
-    
+    }    
     return render(request, "ledger/recipelist.html", ctx1)
+
+def recipe1(request):
+    ctx2 = {
+        "name": "Recipe 1",
+        "ingredients": [
+            {
+                "name": "tomato",
+                "quantity": "3pcs"
+            },
+            {
+                "name": "onion",
+                "quantity": "1pc"
+            },
+            {
+                "name": "pork",
+                "quantity": "1kg"
+            },
+            {
+                "name": "water",
+                "quantity": "1L"
+            },
+            {
+                "name": "sinigang mix",
+                "quantity": "1 packet"
+            }
+        ],
+        "link": "ledger/recipe/list"
+    }
+    return render(request, "ledger/recipe1.html", ctx2)
+
+def recipe2(request):
+    ctx3 = {
+            "name": "Recipe 2",
+            "ingredients": [
+                {
+                    "name": "garlic",
+                    "quantity": "1 head"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "vinegar",
+                    "quantity": "1/2cup"
+                },
+                {
+                    "name": "water",
+                    "quantity": "1 cup"
+                },
+                {
+                    "name": "salt",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "whole black peppers",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1 kilo"
+                }
+            ],
+            "link": "/recipe/2"
+        }
+    return render(request, "ledger/recipe1.html", ctx3)
