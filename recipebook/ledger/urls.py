489193c688763a1c391e12265f5bddbc402c460c
Raphael Fernandez <raphael.tomines.fernandez@gmail>
2026-02-09 10:43:29 +0800

    Added a recipelist

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import homepage
+from .views import homepage, recipelist
 
 urlpatterns = [
     path('', homepage, name='homepage'),
+    path('list', recipelist, name='recipelist'),
 ]
 
 app_name = "ledger"
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,3 +2,71 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, "ledger/homepage.html")
+
+def recipelist(request):
+    ctx1 = {
+        "recipes": [
+            {
+                "name": "Recipe 1",
+                "ingredients": [
+                    {
+                        "name": "tomato",
+                        "quantity": "3pcs"
+                    },
+                    {
+                        "name": "onion",
+                        "quantity": "1pc"
+                    },
+                    {
+                        "name": "pork",
+                        "quantity": "1kg"
+                    },
+                    {
+                        "name": "water",
+                        "quantity": "1L"
+                    },
+                    {
+                        "name": "sinigang mix",
+                        "quantity": "1 packet"
+                    }
+                ],
+                "link": "/recipe/1"
+            },
+            {
+                "name": "Recipe 2",
+                "ingredients": [
+                    {
+                        "name": "garlic",
+                        "quantity": "1 head"
+                    },
+                    {
+                        "name": "onion",
+                        "quantity": "1pc"
+                    },
+                    {
+                        "name": "vinegar",
+                        "quantity": "1/2cup"
+                    },
+                    {
+                        "name": "water",
+                        "quantity": "1 cup"  # Fixed typo: "quanity" → "quantity"
+                    },
+                    {
+                        "name": "salt",
+                        "quantity": "1 tablespoon"
+                    },
+                    {
+                        "name": "whole black peppers",
+                        "quantity": "1 tablespoon"
+                    },
+                    {
+                        "name": "pork",
+                        "quantity": "1 kilo"
+                    }
+                ],
+                "link": "/recipe/2"
+            }
+        ]
+    }
+    
+    return render(request, "ledger/recipelist.html", ctx1)
