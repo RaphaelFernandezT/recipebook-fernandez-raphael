d86256193c747d0ea61903accdda48729b4c001b
Raphael Fernandez <raphael.tomines.fernandez@gmail>
2026-02-19 22:34:37 +0800

    Created models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('ingredient_detail', args=[str(self.name)])
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('recipe_detail', args=[str(self.name)])
+    
+
+class RecipeIngredient(models.Model):
+    quantity = models.DecimalField(max_length=100)
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='Recipe'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='Ingredient'
+    )
+
