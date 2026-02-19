b8e1244f9bed2d1d2527979e14c9774c68822f6b
Raphael Fernandez <raphael.tomines.fernandez@gmail>
2026-02-19 21:34:43 +0800

    removed duplicate templates and views

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from .views import homepage, recipelist, recipe1, recipe2
+from .views import homepage
 
 urlpatterns = [
     path('', homepage, name='homepage'),
-    path('recipes/list', recipelist, name='recipelist'),
-    path('recipe/1', recipe1, name='recipe1'),
-    path('recipe/2', recipe2, name='recipe2'),
 ]
 
 app_name = "ledger"
