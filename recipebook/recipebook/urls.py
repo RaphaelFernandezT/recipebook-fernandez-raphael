004be002b7cf451b8906eb4c073da59b5289bbba
Raphael Fernandez <raphael.tomines.fernandez@gmail>
2026-02-19 21:23:06 +0800

    Corrected links and templates to be project level

--- recipebook/recipebook/urls.py
+++ recipebook/recipebook/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from .views import recipelist, recipe1, recipe2
 
 urlpatterns = [
     path('ledger/', include('ledger.urls', namespace="ledger")),
     path('admin/', admin.site.urls),
+    path('recipes/list', recipelist, name='recipelist'),
+    path('recipe/1', recipe1, name='recipe1'),
+    path('recipe/2', recipe2, name='recipe2'),
 ]
---
+++ recipebook/recipebook/views.py
@@ -0,0 +1,137 @@
+from django.shortcuts import render
+
+def homepage(request):
+    return render(request, "ledger/homepage.html")
+
+def recipelist(request):
+    ctx1 = {
+        "recipes": [
+            {
+                "name": "Recipe 1",
+                "ingredients": [
+                    {
+                        "name": "tomato",
+                        "quantity": "3pcs"
+                    },
+                    {
+                        "name": "onion",
+                        "quantity": "1pc"
+                    },
+                    {
+                        "name": "pork",
+                        "quantity": "1kg"
+                    },
+                    {
+                        "name": "water",
+                        "quantity": "1L"
+                    },
+                    {
+                        "name": "sinigang mix",
+                        "quantity": "1 packet"
+                    }
+                ],
+                "link": "/recipe/1"
+            },
+            {
+                "name": "Recipe 2",
+                "ingredients": [
+                    {
+                        "name": "garlic",
+                        "quantity": "1 head"
+                    },
+                    {
+                        "name": "onion",
+                        "quantity": "1pc"
+                    },
+                    {
+                        "name": "vinegar",
+                        "quantity": "1/2cup"
+                    },
+                    {
+                        "name": "water",
+                        "quantity": "1 cup"  # Fixed typo: "quanity" → "quantity"
+                    },
+                    {
+                        "name": "salt",
+                        "quantity": "1 tablespoon"
+                    },
+                    {
+                        "name": "whole black peppers",
+                        "quantity": "1 tablespoon"
+                    },
+                    {
+                        "name": "pork",
+                        "quantity": "1 kilo"
+                    }
+                ],
+                "link": "/recipe/2"
+            }
+        ]
+    }    
+    return render(request, "recipelist.html", ctx1)
+
+def recipe1(request):
+    ctx2 = {
+        "name": "Recipe 1",
+        "ingredients": [
+            {
+                "name": "tomato",
+                "quantity": "3pcs"
+            },
+            {
+                "name": "onion",
+                "quantity": "1pc"
+            },
+            {
+                "name": "pork",
+                "quantity": "1kg"
+            },
+            {
+                "name": "water",
+                "quantity": "1L"
+            },
+            {
+                "name": "sinigang mix",
+                "quantity": "1 packet"
+            }
+        ],
+        "link": "/recipes/list"
+    }
+    return render(request, "recipe1.html", ctx2)
+
+def recipe2(request):
+    ctx3 = {
+            "name": "Recipe 2",
+            "ingredients": [
+                {
+                    "name": "garlic",
+                    "quantity": "1 head"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "vinegar",
+                    "quantity": "1/2cup"
+                },
+                {
+                    "name": "water",
+                    "quantity": "1 cup"
+                },
+                {
+                    "name": "salt",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "whole black peppers",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1 kilo"
+                }
+            ],
+            "link": "/recipes/list"
+        }
+    return render(request, "recipe1.html", ctx3)
