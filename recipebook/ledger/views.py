ef36131a4d32b1e9478c949242dd26fe80bc49ca
Raphael Fernandez <raphael.tomines.fernandez@gmail>
2026-03-12 20:52:27 +0800

    Modified views.py to add CreateView for Recipes

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,6 +1,8 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView, UpdateView
+from .forms import *
 from .models import *
 
 class RecipeListView(LoginRequiredMixin, ListView):
@@ -13,4 +15,12 @@ class RecipeDetailView(LoginRequiredMixin, DetailView):
     template_name = 'ledger/recipe_detail.html'
     context_object_name = 'recipe'
 
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    form_class = RecipeForm
+
+    def form_invalid(self, form):
+        form.instance.profile = Profile.objects.get(user=self.request.user)
+        return super().form_invalid(form)
 
