8698e959ba4d429c08e48452d7fee35a04f8dad6
Raphael Fernandez <raphael.tomines.fernandez@gmail>
2026-03-12 20:10:41 +0800

    fixed naming of RecipeImage

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -47,5 +47,5 @@ class RecipeImage(models.Model):
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='ingredients'
+        related_name='image'
     )
